528dcefff6c5feef23a0b1a46ae18550af3985ed
Leon Moreti <105951255+leonmoreti@users.noreply.github.com>
2023-03-28 09:45:31 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function trocarImagemETrocarVideo() {
-    document.getElementById("botao").src = "/img/botao-apertado.png";
-    document.getElementById("meu-video").src = "/video/Super.mp4";
+    document.getElementById("botao").src = "./img/botao-apertado.png";
+    document.getElementById("meu-video").src = "./video/Super.mp4";
     document.getElementById("meu-video").loop = false;
     document.getElementById("meu-video").load();
     document.getElementById("meu-video").play();
